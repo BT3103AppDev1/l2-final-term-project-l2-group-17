633ce1f2c68c7410b5ef092addad1a726d613847
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2024-04-01 23:45:13 +0800

    added bootstrap and page header

---
+++ src/components/MajorSelectDropdown.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <select class="form-select">
+      <option disabled selected>Select your major</option>
+      <optgroup label="Arts and Social Sciences">
+        <option value="Economics">Economics</option>
+      </optgroup>
+      <optgroup label="Computing">
+        <option value="Computer Science">Computer Science</option>
+        <option value="Business Analytics">Business Analytics</option>
+        <option value="Information Systems">Information Systems</option>
+      </optgroup>
+      <optgroup label="Science">
+        <option value="Mathematics">Mathematics</option>
+      </optgroup>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "<MajorSelectDropdown>",
+};
+</script>
