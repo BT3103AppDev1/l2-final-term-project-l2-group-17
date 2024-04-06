3400321d760040cb91492c1fd97021baa8a48708
e0968868 <amberacsa@gmail.com>
2024-04-06 15:32:29 +0800

    adjust page layout

--- src/components/ModuleBox.vue
+++ src/components/ModuleBox.vue
@@ -1,48 +1,50 @@
 <template>
-  <!-- div containing the required modules -->
-  <div class="title">Required Modules</div>
-  <div
-    class="box" 
-    @drop="onDrop($event, 1)"
-    @dragenter.prevent
-    @dragover.prevent
-  >
-    <div
-      v-for="item in getList(1)"
-      :key="item.id"
-      class="module"
-      draggable="true"
-      @dragstart="startDrag($event, item)"
-    >
-      <span> {{ item.title }}</span>
-    </div>
-  </div>
-
-  <div class="list-group">
-    <button
-      v-for="(requirement, index) in requirements"
-      :key="index"
-      type="button"
-      class="list-group-item list-group-item-action"
-      @click="toggleContent(index)"
-    >
-      {{ requirement.name }}
-      <span v-if="!requirement.showContent" class="float-end">See more</span>
-      <span v-else class="float-end">See less</span>
-    </button>
-    <div
-      v-for="(requirement, index) in requirements"
-      :key="'content-' + index"
-      :class="{ expanded: requirement.showContent }"
-    >
-      <div v-if="requirement.showContent">
-        <!-- Content for this requirement -->
-        {{ requirement.content }}
+  <div class="app-container"> 
+    <div class="left-column">
+      <div class="title">Required Modules</div>
+      <div
+        class="box"
+        @drop="onDrop($event, 1)"
+        @dragenter.prevent
+        @dragover.prevent
+      >
+        <div
+          v-for="item in getList(1)"
+          :key="item.id"
+          class="module"
+          draggable="true"
+          @dragstart="startDrag($event, item)"
+        >
+          <span> {{ item.title }}</span>
+        </div>
       </div>
-    </div>
-  </div>
 
-  <div class="year-container">
+      <div class="list-group">
+        <button
+          v-for="(requirement, index) in requirements"
+          :key="index"
+          type="button"
+          class="list-group-item list-group-item-action"
+          @click="toggleContent(index)"
+        >
+          {{ requirement.name }}
+          <span v-if="!requirement.showContent" class="float-end">See more</span>
+          <span v-else class="float-end">See less</span>
+        </button>
+        <div
+          v-for="(requirement, index) in requirements"
+          :key="'content-' + index"
+          :class="{ expanded: requirement.showContent }"
+        >
+          <div v-if="requirement.showContent">
+            <!-- Content for this requirement -->
+            {{ requirement.content }}
+          </div>
+        </div>
+      </div>
+    </div> 
+    <div class="right-column">
+    <div class="year-container">
     <div class="title">Study Plan</div>
     <div class="year-box">
       <div
@@ -88,40 +90,16 @@
       </div>
     </div>
   </div>
+</div>
+</div>
+
 </template>
 
+
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 export default {
-  data() {
-    return {
-      requirements: [
-        {
-          name: "Common Curriculum",
-          content: "Content for Common Curriculum",
-          showContent: false,
-        },
-        {
-          name: "Unrestricted Electives",
-          content: "Content for Unrestricted Electives",
-          showContent: false,
-        },
-        {
-          name: "Core Major Modules",
-          content: "Content for Core Major Modules",
-          showContent: false,
-        },
-        // Add more requirements as needed
-      ],
-    };
-  },
-  methods: {
-    toggleContent(index) {
-      this.requirements[index].showContent =
-        !this.requirements[index].showContent;
-    },
-  },
   setup() {
     const items = ref([
       { id: 0, title: "Module A", list: 1, MCs: 4 },
@@ -129,42 +107,42 @@ export default {
       { id: 2, title: "Module C", list: 2, MCs: 4 },
     ]);
 
-    const getList = (list) => {
-      return items.value.filter((item) => item.list === list);
-    };
+    const requirements = reactive([
+      { name: "Common Curriculum", content: "Content for Common Curriculum", showContent: false },
+      { name: "Unrestricted Electives", content: "Content for Unrestricted Electives", showContent: false },
+      { name: "Core Major Modules", content: "Content for Core Major Modules", showContent: false },
+    ]);
+
+    const getList = (list) => items.value.filter((item) => item.list === list);
 
     const startDrag = (event, item) => {
-      event.dataTransfer.dropEffect = "move";
-      event.dataTransfer.effectAllowed = "move";
-      event.dataTransfer.setData("itemID", item.id);
+      event.dataTransfer.setData("itemID", item.id.toString());
     };
 
     const onDrop = (event, list) => {
-      const itemID = event.dataTransfer.getData("itemID");
-      const item = items.value.find((item) => item.id === parseInt(itemID));
-      if (item.list !== list) {
+      event.preventDefault();
+      const itemID = parseInt(event.dataTransfer.getData("itemID"));
+      const item = items.value.find((item) => item.id === itemID);
+      if (item && item.list !== list) {
         item.list = list;
       }
     };
 
     const removeModule = (item) => {
-      item.list = 1; // Move the item back to the first list
+      item.list = 1;  
     };
 
-    const mcCount = (list) => {
-      return getList(list).reduce((total, item) => total + item.MCs, 0);
+    const mcCount = (list) => getList(list).reduce((total, item) => total + item.MCs, 0);
+
+    const toggleContent = (index) => {
+      requirements[index].showContent = !requirements[index].showContent;
     };
 
-    return {
-      getList,
-      onDrop,
-      startDrag,
-      removeModule,
-      mcCount,
-    };
+    return { getList, startDrag, onDrop, removeModule, mcCount, toggleContent, requirements };
   },
 };
 </script>
+
 
 <style>
 .req-modules {
@@ -219,12 +197,12 @@ export default {
 .year-box {
   display: flex;
 }
-
 .semester-box {
   margin: 0 10px;
   padding: 80px;
+  border: 2px dashed #ccc;  
+  min-height: 200px;  
 }
-
 .mc-indicator {
   position: absolute;
   bottom: 5px;
@@ -244,4 +222,20 @@ export default {
 .expanded {
   height: auto !important;
 }
+
+.app-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;  
+  width: 100%;  
+}
+
+.left-column, .right-column {
+  flex: 1; 
+} 
+.year-container {
+  width: 100%;
+  padding: 10px;
+}
+
 </style>
--- src/router.js
+++ src/router.js
@@ -3,15 +3,17 @@ import Login from './components/Login.vue';
 import SignUp from './components/SignUp.vue';
 import ForgotPassword from './views/ForgotPassword.vue';
 import Logout from './components/Logout.vue';
+import ModuleBox from './components/ModuleBox.vue';
 // import Home from './views/Home.vue';
 
 const routes = [
   // { path: '/', component: Home },
-  { path: '/', redirect : '/login'},
+  { path: '/', redirect : '/login'}, 
   { path: '/login', component: Login, alias: '/' },
   { path: '/signup', component: SignUp },
   { path: '/forgot-password', component: ForgotPassword },
   { path: '/logout', component: Logout},
+  { path: '/moduleplanning', component: ModuleBox }
 ];
 
 const router = createRouter({
