f404f4edaab0c4ecfd67c9a073aed7383a4143ef
e0968868 <amberacsa@gmail.com>
2024-04-09 10:57:07 +0800

    Fetch department and major in dropdown menu

--- src/components/MajorSelectDropdown.vue
+++ src/components/MajorSelectDropdown.vue
@@ -1,24 +1,61 @@
 <template>
   <div>
-    <select class="form-select">
-      <option disabled selected>Select your major</option>
-      <optgroup label="Arts and Social Sciences">
-        <option value="Economics">Economics</option>
-      </optgroup>
-      <optgroup label="Computing">
-        <option value="Computer Science">Computer Science</option>
-        <option value="Business Analytics">Business Analytics</option>
-        <option value="Information Systems">Information Systems</option>
-      </optgroup>
-      <optgroup label="Science">
-        <option value="Mathematics">Mathematics</option>
+    <select class="form-select" v-model="selectedMajor">
+      <option disabled value="">Select your major</option>
+      <optgroup v-for="faculty in faculties" :key="faculty.name" :label="faculty.name">
+        <option v-for="department in faculty.departments" :key="department" :value="department">
+          {{ department }}
+        </option>
       </optgroup>
     </select>
   </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+
 export default {
-  name: "<MajorSelectDropdown>",
+  name: "MajorSelectDropdown",
+  setup() {
+    const faculties = ref([]);
+
+    const selectedMajor = ref("");
+
+    onMounted(async () => {
+      try {
+        const response = await fetch('https://api.nusmods.com/v2/2023-2024/moduleInfo.json');
+        if (!response.ok) throw new Error('Failed to fetch modules');
+        const modules = await response.json();
+
+        const facultyDepartmentMap = new Map();
+
+        for (const module of modules) {
+          if (!facultyDepartmentMap.has(module.faculty)) {
+            facultyDepartmentMap.set(module.faculty, new Set([module.department]));
+          } else {
+            facultyDepartmentMap.get(module.faculty).add(module.department);
+          }
+        }
+
+        const facultyData = [];
+        facultyDepartmentMap.forEach((departments, faculty) => {
+          facultyData.push({
+            name: faculty,
+            departments: [...departments].sort(),
+          });
+        });
+
+        faculties.value = facultyData.sort((a, b) => a.name.localeCompare(b.name));
+
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    return {
+      faculties,
+      selectedMajor,
+    };
+  },
 };
 </script>
