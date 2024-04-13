c1eb5d37ff7065ae7cebaf9444445c7a5086697b
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2024-04-14 02:39:31 +0800

    created module planning page header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <template>
   <div id="app" class="container">
-    <header class="row align-items-center" style="padding-top: 30px">
-      <div class="col-auto">
-        <img src="./assets/modunus_logo.jpeg" alt="Logo" class="logo" />
-      </div>
-      <div class="col">
-        <MajorSelectDropdown />
-      </div>
-      <div class="col-auto">
-        <button class="btn btn-primary">Load Course Requirements</button>
-      </div>
-    </header>
+    <ModulePlanningHeader />
+    <ModuleBox />
   </div>
-  <ModuleBox />
 </template>
 
 <style>
@@ -24,13 +14,13 @@
 
 <script>
 import ModuleBox from "./components/ModuleBox.vue";
-import MajorSelectDropdown from "./components/MajorSelectDropdown.vue";
+import ModulePlanningHeader from "./components/ModulePlanningHeader.vue";
 
 export default {
   name: "App",
   components: {
     ModuleBox,
-    MajorSelectDropdown,
+    ModulePlanningHeader,
   },
 };
 </script>
---
+++ src/components/ModulePlanningHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <header class="row align-items-center" style="padding-top: 30px">
+      <div class="col-auto">
+        <img src="../assets/modunus_logo.jpeg" alt="Logo" class="logo" />
+      </div>
+      <div class="col">
+        <MajorSelectDropdown />
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-primary">Load Course Requirements</button>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+import MajorSelectDropdown from "./MajorSelectDropdown.vue";
+
+export default {
+  name: "ModulePlanningHeader",
+  components: {
+    MajorSelectDropdown,
+  },
+};
+</script>
