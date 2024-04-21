356c63d7098514a2f27248e948c3bd0183a19eb3
yuzujy <e0968312@u.nus.edu>
2024-04-22 01:00:18 +0800

    Update UI for Community banner

--- src/components/CommunityHeader.vue
+++ src/components/CommunityHeader.vue
@@ -5,12 +5,13 @@
           <div class="col-md-7">
             <h1 class="display-5 fw-bold">The best study resources.</h1>
             <p class="lead">By you, for you.</p>
-            <button @click="openModal" class="btn btn-primary btn-lg">
-                <i class="bi bi-plus-lg"></i> Make your post today
+            <button @click="openModal" class="btn btn-primary lg-3">
+              <font-awesome-icon :icon="['fas', 'plus']" /> Make your post today
+              <!-- <i class="bi bi-plus-lg"></i> Make your post today -->
             </button>
           </div>
           <div class="col-md-5 d-none d-md-block">
-            <img src="../../assets/learning_icon.png" alt="Learning Icon" class="img-fluid">
+            <img src="../../assets/learning_icon.png" alt="Learning Icon" class="img-fluid" style="max-width: 84%;">
           </div>
         </div>
       </div>
@@ -18,7 +19,16 @@
   </template>
   
   <script>
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+  import { library } from '@fortawesome/fontawesome-svg-core';
+  import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+  library.add(faPlus);
+
   export default {
+    components: {
+    FontAwesomeIcon,
+  },
     name: 'CommunityHeader',
     methods: {
         openModal() {
@@ -33,6 +43,7 @@
     background-color: #e7f2ff;
     padding: 2rem 0;
     margin: 1rem 0;
+    border-radius: 10px;
     margin-top: 0;
   }
   </style>
